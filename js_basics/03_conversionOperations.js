e633deeebb8fd5325607fc7afcde1fbeba75a85d
Aditya Kumbhar <112404922+AdityaKumbhar21@users.noreply.github.com>
2023-09-30 06:41:23 +0000

    conversions

---
+++ js_basics/03_conversionOperations.js
@@ -0,0 +1,28 @@
+let age = true
+
+// to convert from any datatype we can use the following syntax
+let numberAge = Number(age)
+// console.log(typeof numberAge);
+// console.log(numberAge);
+
+// "18" -> number -> 18
+// "18axaxsx" ->number -> NaN (Not a Number)
+//"Aditya"-> number -> NaN
+// true -> 1 ; false -> 0;
+
+
+let isLoggedIn = 1
+
+let boolIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof boolIsLoggedIn);
+// console.log(boolIsLoggedIn);
+
+// 1 -> boolean -> true
+// 0 -> boolean -> false
+
+let score = 199
+let stringScore = String(score)
+console.log(typeof stringScore);
+console.log(stringScore);
+
+// 199 -> "199"
